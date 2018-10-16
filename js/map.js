c8c2c6e9509f1e79d6165caff6d692de7617a381
Vita <vitalia@myrambler.ru>
2018-10-16 10:30:09 +0300

    simplification

--- js/map.js
+++ js/map.js
@@ -6,12 +6,6 @@
   var pinContainer = map.querySelector('.map__pins');
   var mapPinMain = map.querySelector('.map__pin--main');
   var startCoords = {};
-  var limits = {
-    top: 130 - MAIN_PIN_TAIL_HEIGHT - mapPinMain.clientHeight,
-    left: 1,
-    right: 1135,
-    bottom: 630 - MAIN_PIN_TAIL_HEIGHT - mapPinMain.clientHeight
-  };
   var defaultCoords = {
     x: 570,
     y: 375
@@ -77,37 +71,26 @@
   }
 
   function onDocumentMouseMove(evt) {
+    evt.preventDefault();
+    //find cursor's displacement during one event
+    //the difference between start coords and current coords
     var shift = {
       x: startCoords.x - evt.clientX,
       y: startCoords.y - evt.clientY
     };
+    //rewrite start location
     startCoords = {
       x: evt.clientX,
       y: evt.clientY
     };
-    var newCoords = calculateNewCoords(shift);
-    setCoords(newCoords);
-  }
-
-  function calculateNewCoords(shift) {
-    var newCoords = {
+    //add the difference thus moving the pin on cursor's place 
+   var newCoords = {
       x: mapPinMain.offsetLeft - shift.x,
       y: mapPinMain.offsetTop - shift.y
     };
-    if (newCoords.x > limits.right) {
-      newCoords.x = limits.right;
-    }
-    if (newCoords.x < limits.left) {
-      newCoords.x = limits.left;
-    }
-    if (newCoords.y > limits.bottom) {
-      newCoords.y = limits.bottom;
-    }
-    if (newCoords.y < limits.top) {
-      newCoords.y = limits.top;
-    }
-    return newCoords;
+    setCoords(newCoords);
   }
+ 
 
   function setCoords(coords) {
     mapPinMain.style.left = coords.x + 'px';
